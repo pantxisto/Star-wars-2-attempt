814107b965c056d37e5fb3a11a3eb88b7489f50d
jonandonicastelo <jonandonicasteloinf@outlook.com>
2020-08-19 20:30:39 +0200

    Sistema de cache para peticiones http

--- src/app/app-interceptor.interceptor.ts
+++ src/app/app-interceptor.interceptor.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
+import * as moment from 'moment';
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
@@ -17,21 +18,28 @@ export class AppInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     let modifiedReq;
-    const globalCookie = this.cookieService.get('globals');
-    const parsedGlobalCookie = globalCookie ? JSON.parse(globalCookie) : {};
-    if (!parsedGlobalCookie['currentUser']) {
-      modifiedReq = request.clone({
-        headers: request.headers.set(
-          'Authorization',
-          'Basic ' + parsedGlobalCookie['currentUser']['authdata']
-        ),
-      });
-    } else {
-      modifiedReq = request.clone({
-        headers: request.headers.set('Authorization', 'Basic'),
-      });
+    const storedRequest = localStorage.getItem(request.url);
+    const expirationDate = storedRequest
+      ? moment(storedRequest).add(5, 'minutes')
+      : null;
+    if (expirationDate?.isBefore(moment()) || !storedRequest) {
+      localStorage.setItem(request.url, moment().toDate().toISOString());
+      const globalCookie = this.cookieService.get('globals');
+      const parsedGlobalCookie = globalCookie ? JSON.parse(globalCookie) : {};
+      if (!parsedGlobalCookie['currentUser']) {
+        modifiedReq = request.clone({
+          headers: request.headers.set(
+            'Authorization',
+            'Basic ' + parsedGlobalCookie['currentUser']['authdata']
+          ),
+        });
+      } else {
+        modifiedReq = request.clone({
+          headers: request.headers.set('Authorization', 'Basic'),
+        });
+      }
+      console.log(modifiedReq);
+      return next.handle(modifiedReq);
     }
-    console.log(modifiedReq);
-    return next.handle(modifiedReq);
   }
 }
--- src/app/ships/ships-interceptor.interceptor.ts
+++ src/app/ships/ships-interceptor.interceptor.ts
@@ -6,19 +6,26 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
+import * as moment from 'moment';
 
 @Injectable()
 export class ShipsInterceptor implements HttpInterceptor {
-  constructor(private cookieService: CookieService) {}
+  constructor() {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    const modifiedReq = request.clone({
-      headers: request.headers.set('Authorization', 'none'),
-    });
-    return next.handle(modifiedReq);
+    const storedRequest = localStorage.getItem(request.url);
+    const expirationDate = storedRequest
+      ? moment(storedRequest).add(5, 'minutes')
+      : null;
+    if (expirationDate?.isBefore(moment()) || !storedRequest) {
+      localStorage.setItem(request.url, moment().toDate().toISOString());
+      const modifiedReq = request.clone({
+        headers: request.headers.set('Authorization', 'none'),
+      });
+      return next.handle(modifiedReq);
+    }
   }
 }
